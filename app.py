8fa10c32ec347b6574a009059b54f9914d47797a
humjaisa <lifeeditor963@gmail.com>
2025-06-19 11:51:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
-from flask import Flask, request, jsonify
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "HumJaisa AI is running!"
-
-@app.route('/ask', methods=['POST'])
-def ask():
-    user_input = request.json.get('message', '')
-    return jsonify({"response": f"HumJaisa AI ka jawab: {user_input[::-1]}"})  # bas demo
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    return render_template('index.html')
