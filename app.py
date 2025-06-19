7a5d5b1db93db6b8b6db2deabc66e8bc3762ca8b
humjaisa <lifeeditor963@gmail.com>
2025-06-19 17:49:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,14 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
-    user_msg = request.args.get('msg')
-    if not user_msg:
-        return jsonify({"error": "Message missing"}), 400
-    emotions = t2e.get_emotion(user_msg)
-    return jsonify(emotions)
+    try:
+        user_msg = request.args.get('msg')
+        if not user_msg:
+            return jsonify({"error": "Message missing"}), 400
+        emotions = t2e.get_emotion(user_msg)
+        return jsonify(emotions)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
