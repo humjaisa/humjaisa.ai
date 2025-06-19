7997e41b1b31beab1bd7cca4af3c896b5cd82351
humjaisa <lifeeditor963@gmail.com>
2025-06-19 17:38:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, render_template
+import text2emotion as t2e
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/get-response', methods=['POST'])
-def get_response():
-    data = request.get_json()
-    user_message = data.get('message', '')
+@app.route("/get")
+def get_bot_response():
+    user_msg = request.args.get('msg')
+    emotions = t2e.get_emotion(user_msg)
+    return str(emotions)
 
-    if "/hello" in user_message.lower():
-        reply = "Hello! 👋 I'm always here to help."
-    else:
-        reply = "I'm learning. Type /hello to start!"
-
-    return jsonify({'reply': reply})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
